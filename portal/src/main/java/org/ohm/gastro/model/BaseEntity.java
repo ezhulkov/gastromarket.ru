dcc21cb640c65f50901f6e8b2bb24222e788c902
ezhulkov <ezhulkov@gmail.com>
2014-08-21 23:47:16 +0400

    - Initial upload

---
+++ portal/src/main/java/org/ohm/gastro/model/BaseEntity.java
@@ -0,0 +1,10 @@
+package org.ohm.gastro.model;
+
+/**
+ * Created by ezhulkov on 21.08.14.
+ */
+public interface BaseEntity {
+
+    public Long getId();
+
+}
---
+++ portal/src/main/java/org/ohm/gastro/portal/AppModule.java
@@ -0,0 +1,7 @@
+package org.ohm.gastro.portal;
+
+/**
+ * Created by ezhulkov on 21.08.14.
+ */
+public class AppModule {
+}
