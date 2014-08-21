18ab32c7a0401e2522fcf5ff37f8b2325f6e3e94
ezhulkov <ezhulkov@gmail.com>
2014-08-21 23:55:34 +0400

    - Initial upload

---
+++ gastro-portal/src/main/java/org/ohm/gastro/reps/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.ohm.gastro.reps;
+
+/**
+ * Created by ezhulkov on 21.08.14.
+ */
+public interface ProductRepository {
+}
---
+++ gastro-portal/src/main/java/org/ohm/gastro/service/ProductService.java
@@ -0,0 +1,7 @@
+package org.ohm.gastro.service;
+
+/**
+ * Created by ezhulkov on 21.08.14.
+ */
+public interface ProductService {
+}
