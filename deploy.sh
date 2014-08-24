343013fdcb31742a253e080d96576865e396097b
ezhulkov <ezhulkov@gmail.com>
2014-08-24 15:24:50 +0400

    - Users

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mvn clean install -DskipTests=true -P prod
-ansible-playbook -i bootstrap/ansible/prod bootstrap/ansible/deploy.yml -u ezhulkov
+ansible-playbook -i bootstrap/ansible/prod bootstrap/ansible/deploy.yml
 
