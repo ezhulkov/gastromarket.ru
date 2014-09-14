6a0e214c1509c40d1c6300f0aa5f69223ed4a291
ezhulkov <ezhulkov@gmail.com>
2014-09-14 19:59:18 +0400

    - Edit in progress

--- gastro-portal/src/main/java/org/ohm/gastro/gui/pages/catalog/Index.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/pages/catalog/Index.java
@@ -1,7 +1,10 @@
 package org.ohm.gastro.gui.pages.catalog;
 
 import org.apache.tapestry5.annotations.Cached;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.TextArea;
+import org.apache.tapestry5.corelib.components.TextField;
 import org.ohm.gastro.domain.ProductEntity;
 import org.ohm.gastro.domain.TagEntity;
 import org.ohm.gastro.domain.UserEntity;
@@ -21,6 +24,15 @@ public class Index extends BaseComponent {
     @Property
     private TagEntity oneTag;
 
+    @Component(id = "name", parameters = {"value=product.name", "validate=required"})
+    private TextField nameField;
+
+    @Component(id = "description", parameters = {"value=product.description"})
+    private TextArea descField;
+
+    @Component(id = "price", parameters = {"value=product.price"})
+    private TextField priceField;
+
     public boolean onActivate(Long id) {
         return onActivate(id, "");
     }
@@ -31,6 +43,10 @@ public class Index extends BaseComponent {
         return true;
     }
 
+    public Object[] onPassivate() {
+        return edit ? new Object[]{product.getId(), true} : new Object[]{product.getId()};
+    }
+
     @Cached
     public java.util.List<TagEntity> getProductTags() {
         return getProductTags(product);
@@ -39,6 +55,10 @@ public class Index extends BaseComponent {
     public boolean isOwner() {
         UserEntity user = getAuthenticatedUser();
         return user != null && product.getCatalog().getUser().equals(user);
+    }
+
+    public void onSubmitFromEditForm() {
+        getCatalogService().saveProduct(product);
     }
 
 }
--- gastro-portal/src/main/java/org/ohm/gastro/service/impl/CatalogServiceImpl.java
+++ gastro-portal/src/main/java/org/ohm/gastro/service/impl/CatalogServiceImpl.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -60,6 +61,8 @@ public class CatalogServiceImpl implements CatalogService {
 
     @Override
     public List<ProductEntity> searchProducts(String query) {
+        if (StringUtils.isEmpty(query)) return Collections.emptyList();
+        query = query.replaceAll("\\s", "|");
         return productRepository.searchProducts(query);
     }
 
