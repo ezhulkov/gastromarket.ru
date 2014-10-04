08f888decc18b81dd51d8d849ed24105adbd873f
ezhulkov <ezhulkov@gmail.com>
2014-10-04 16:48:46 +0400

    - Hier cats done

--- gastro-portal/src/main/java/org/ohm/gastro/gui/pages/chef/Index.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/pages/chef/Index.java
@@ -1,20 +1,20 @@
 package org.ohm.gastro.gui.pages.chef;
 
-import org.apache.tapestry5.EventContext;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.TextField;
 import org.ohm.gastro.domain.CatalogEntity;
 import org.ohm.gastro.domain.ProductEntity;
+import org.ohm.gastro.gui.AbstractServiceCallback;
+import org.ohm.gastro.gui.ServiceCallback;
 import org.ohm.gastro.gui.components.Catalog;
 import org.ohm.gastro.gui.mixins.BaseComponent;
+import org.ohm.gastro.gui.pages.EditObjectPage;
 
 /**
  * Created by ezhulkov on 31.08.14.
  */
-public class Index extends BaseComponent {
-
-    @Property
-    private CatalogEntity catalog;
+public class Index extends EditObjectPage<CatalogEntity> {
 
     @Property
     private ProductEntity oneProduct;
@@ -22,15 +22,41 @@ public class Index extends BaseComponent {
     @Component(id = "catalog")
     private Catalog catalogComponent;
 
-    public Class onActivate(EventContext context) {
-        if (context.getCount() == 0) return List.class;
-        catalog = getCatalogService().findCatalog(context.get(Long.class, 0));
-        catalogComponent.activate(catalog, null, null, false);
-        return null;
+    @Component(id = "name", parameters = {"value=object?.name", "validate=maxlength=64,required"})
+    private TextField nameField;
+
+    @Override
+    public void activated() {
+        catalogComponent.activate(getObject(), null, null, false);
     }
 
-    public Object[] onPassivate() {
-        return catalog == null ? null : new Object[]{catalog.getId()};
-    }
+    @Override
+    public ServiceCallback<CatalogEntity> getServiceCallback() {
+        return new AbstractServiceCallback<CatalogEntity>() {
 
+            @Override
+            public Class<? extends BaseComponent> updateObject(CatalogEntity object) {
+                getCatalogService().saveCatalog(object);
+                return Index.class;
+            }
+
+            @Override
+            public Class<? extends BaseComponent> addObject(CatalogEntity object) {
+                getCatalogService().saveCatalog(object);
+                return Index.class;
+            }
+
+            @Override
+            public CatalogEntity newObject() {
+                CatalogEntity catalog = new CatalogEntity();
+                catalog.setUser(getAuthenticatedUser());
+                return catalog;
+            }
+
+            @Override
+            public CatalogEntity findObject(String id) {
+                return getCatalogService().findCatalog(Long.parseLong(id));
+            }
+        };
+    }
 }
