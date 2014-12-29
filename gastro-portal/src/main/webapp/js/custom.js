b3f2c9ce68e39b42bc79434789976441c319b9d3
ezhulkov <ezhulkov@gmail.com>
2014-12-30 01:16:09 +0300

    - GUI

---
+++ gastro-portal/src/main/webapp/js/custom.js
@@ -0,0 +1,34 @@
+(function () {
+    var isBootstrapEvent = false;
+    if (window.jQuery) {
+        var all = jQuery('*');
+        jQuery.each(['hide.bs.dropdown',
+            'hide.bs.collapse',
+            'hide.bs.modal',
+            'hide.bs.tooltip',
+            'hide.bs.popover'], function (index, eventName) {
+            all.on(eventName, function (event) {
+                isBootstrapEvent = true;
+            });
+        });
+    }
+    var originalHide = Element.hide;
+    Element.addMethods({
+        hide: function (element) {
+            if (isBootstrapEvent) {
+                isBootstrapEvent = false;
+                return element;
+            }
+            return originalHide(element);
+        }
+    });
+})();
+jQuery(document).ready(function () {
+    jQuery("ul.dropdown-menu").each(function (e) {
+        var parentWidth = jQuery(this).parent().innerWidth();
+        var menuWidth = jQuery(this).innerWidth();
+        var margin = (parentWidth / 2 ) - (menuWidth / 2);
+        margin = margin + "px";
+        jQuery(this).css("margin-left", margin);
+    });
+});
