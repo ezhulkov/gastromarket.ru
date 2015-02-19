598234e7b58444f896baaf4315c43543cbdf2094
ezhulkov <ezhulkov@gmail.com>
2015-02-19 17:07:53 +0300

    - initial data

--- bootstrap/database/storage.sql
+++ bootstrap/database/storage.sql
@@ -1,18 +1,18 @@
-create role gastro login inherit createdb createrole;
-alter role gastro password 'gastro';
-create database gastro_prod owner gastro template template0 LC_COLLATE 'ru_RU.UTF-8' LC_CTYPE 'ru_RU.UTF-8';
+CREATE ROLE gastro LOGIN INHERIT CREATEDB CREATEROLE;
+ALTER ROLE gastro PASSWORD 'gastro';
+CREATE DATABASE gastro_prod OWNER gastro TEMPLATE template0 LC_COLLATE 'ru_RU.UTF-8' LC_CTYPE 'ru_RU.UTF-8';
 
 -- place russian* to /usr/share/postgresql/9.4/tsearch_data
 
 CREATE TEXT SEARCH DICTIONARY russian_ispell (
-    TEMPLATE = ispell,
-    DictFile = russian,
-    AffFile = russian,
-    StopWords = russian
+TEMPLATE = ispell,
+  DictFile = russian,
+  AffFile = russian,
+  StopWords = russian
 );
 CREATE TEXT SEARCH CONFIGURATION ru ( COPY = russian );
 ALTER TEXT SEARCH CONFIGURATION ru ALTER MAPPING FOR hword, hword_part, word WITH russian_ispell, russian_stem;
-select to_tsvector('ru', 'мама мыла раму');
+SELECT to_tsvector('ru', 'мама мыла раму');
 
 -- http://dolzhenko.blogspot.ru/2008/02/postgresql-83-full-text-search.html
 -- http://fmg-www.cs.ucla.edu/geoff/ispell-dictionaries.html#Russian-dicts
