a156f3c2f9fe381e2691fabbf10253fc93e22328
ezhulkov <ezhulkov@gmail.com>
2015-03-24 23:08:57 +0300

    - AltId done

--- gastro-portal/src/main/java/org/ohm/gastro/util/CommonsUtils.java
+++ gastro-portal/src/main/java/org/ohm/gastro/util/CommonsUtils.java
@@ -77,6 +77,16 @@ public class CommonsUtils {
             put('x', "x");
             put('y', "y");
             put('z', "z");
+            put('0', "0");
+            put('1', "1");
+            put('2', "2");
+            put('3', "3");
+            put('4', "4");
+            put('5', "5");
+            put('6', "6");
+            put('7', "7");
+            put('8', "8");
+            put('9', "9");
         }
     };
 
