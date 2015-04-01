1866f144b8128d060796454fb1bd0fe328592c47
ezhulkov <ezhulkov@gmail.com>
2015-04-01 08:52:09 +0300

    - Some work

--- gastro-portal/src/main/java/org/ohm/gastro/gui/pages/CookLicense.java
+++
@@ -1,9 +0,0 @@
-package org.ohm.gastro.gui.pages;
-
-import org.ohm.gastro.gui.mixins.BaseComponent;
-
-/**
- * Created by ezhulkov on 23.08.14.
- */
-public class CookLicense extends BaseComponent {
-}
