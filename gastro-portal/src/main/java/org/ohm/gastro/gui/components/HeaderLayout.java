3ea260f8dd6fdda7d16dc933fc064e59eb9814a8
ezhulkov <ezhulkov@gmail.com>
2015-04-03 19:29:56 +0300

    - Terms fixed

--- gastro-portal/src/main/java/org/ohm/gastro/gui/components/HeaderLayout.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/HeaderLayout.java
@@ -1,12 +1,10 @@
 package org.ohm.gastro.gui.components;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.ohm.gastro.domain.CatalogEntity;
-import org.ohm.gastro.domain.UserEntity;
 import org.ohm.gastro.gui.mixins.BaseComponent;
 
 import java.util.List;
@@ -66,14 +64,7 @@ public class HeaderLayout extends BaseComponent {
     }
 
     public String getAvatarUrl() {
-        if (!isCook()) {
-            return getAuthenticatedUser().getAvatarUrlSmall();
-        } else {
-            final UserEntity user = getAuthenticatedUser();
-            final List<CatalogEntity> allCatalogs = getCatalogService().findAllCatalogs(user);
-            if (CollectionUtils.isEmpty(allCatalogs)) return user.getAvatarUrlSmall();
-            else return allCatalogs.get(0).getAvatarUrlSmall();
-        }
+        return getAuthenticatedUser().getAvatarUrlSmall();
     }
 
 }
