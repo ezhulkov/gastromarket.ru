180f5af6cd6ce46aa3dd60f2d4802303b91d4293
ezhulkov <ezhulkov@gmail.com>
2015-08-03 18:58:27 +0300

    - cart in progress

--- gastro-portal/src/main/java/org/ohm/gastro/gui/pages/Cart.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/pages/Cart.java
@@ -1,10 +1,23 @@
 package org.ohm.gastro.gui.pages;
 
+import org.apache.tapestry5.annotations.Property;
+import org.ohm.gastro.domain.CatalogEntity;
 import org.ohm.gastro.gui.mixins.BaseComponent;
+
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 /**
  * Created by ezhulkov on 23.08.14.
  */
 public class Cart extends BaseComponent {
+
+    @Property
+    private CatalogEntity catalog;
+
+    public List<CatalogEntity> getCatalogs() {
+        return getShoppingCart().getCatalogs().stream().map(Entry::getKey).collect(Collectors.toList());
+    }
 
 }
