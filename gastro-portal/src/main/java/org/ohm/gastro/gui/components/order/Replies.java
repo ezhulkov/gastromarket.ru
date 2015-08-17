7e7ec3367c45a07ae015fd55fe463776e20411e0
ezhulkov <ezhulkov@gmail.com>
2015-08-17 20:55:31 +0300

    - tender comments

--- gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/Replies.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/Replies.java
@@ -1,13 +1,22 @@
 package org.ohm.gastro.gui.components.order;
 
+import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.Cached;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
 import org.ohm.gastro.domain.CommentEntity;
 
 /**
  * Created by ezhulkov on 31.07.15.
  */
 public class Replies extends AbstractOrder {
+
+    @Inject
+    @Property
+    private Block replyBlock;
+
+    @Property
+    private String replyText;
 
     @Property
     private CommentEntity oneComment;
