00eb754090646047ee4cd3a0ebe0772800b4ab16
ezhulkov <ezhulkov@gmail.com>
2015-09-08 00:01:59 +0300

    - landing

--- gastro-portal/src/main/java/org/ohm/gastro/gui/pages/Index.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/pages/Index.java
@@ -1,21 +1,14 @@
 package org.ohm.gastro.gui.pages;
 
 import org.apache.tapestry5.EventContext;
-import org.apache.tapestry5.Link;
 import org.apache.tapestry5.annotations.Cached;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.HttpError;
-import org.apache.tapestry5.services.URLEncoder;
 import org.ohm.gastro.domain.CatalogEntity;
 import org.ohm.gastro.domain.OrderEntity;
 import org.ohm.gastro.domain.OrderEntity.Status;
-import org.ohm.gastro.domain.ProductEntity;
-import org.ohm.gastro.domain.PropertyValueEntity;
 import org.ohm.gastro.gui.mixins.BaseComponent;
-import org.ohm.gastro.gui.pages.product.Search;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,20 +17,8 @@ import java.util.stream.Collectors;
  */
 public class Index extends BaseComponent {
 
-    @Inject
-    private URLEncoder urlEncoder;
-
-    @Property
-    private String searchString = "";
-
-    @Property
-    private PropertyValueEntity onePropertyValue;
-
     @Property
     private CatalogEntity oneCook;
-
-    @Property
-    private ProductEntity oneProduct;
 
     @Property
     private OrderEntity oneTender;
@@ -48,24 +29,10 @@ public class Index extends BaseComponent {
     }
 
     @Cached
-    public List<PropertyValueEntity> getPropertyValues() {
-        return getPropertyService().findAllValues(PropertyValueEntity.Tag.ROOT);
-    }
-
-    @Cached
     public List<CatalogEntity> getCooks() {
         return getCatalogService().findAllActiveCatalogs().stream()
                 .sorted(((o1, o2) -> o2.getRating().compareTo(o1.getRating())))
                 .limit(5).collect(Collectors.toList());
-    }
-
-    @Cached
-    public List<ProductEntity> getProducts() {
-        return getProductService().findPromotedProducts().stream().limit(4).collect(Collectors.toList());
-    }
-
-    public Link onSubmitFromSearchForm() throws IOException {
-        return getPageLinkSource().createPageRenderLinkWithContext(Search.class, Search.processSearchString(searchString));
     }
 
     @Cached
