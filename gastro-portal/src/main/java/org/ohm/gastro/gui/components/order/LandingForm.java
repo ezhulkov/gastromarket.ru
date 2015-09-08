b38e209add55efaf98e73bff34ed4ed73342478b
ezhulkov <ezhulkov@gmail.com>
2015-09-08 10:12:17 +0300

    - landing

---
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/LandingForm.java
@@ -0,0 +1,10 @@
+package org.ohm.gastro.gui.components.order;
+
+import org.ohm.gastro.gui.mixins.BaseComponent;
+
+/**
+ * Created by ezhulkov on 31.07.15.
+ */
+public class LandingForm extends BaseComponent {
+
+}
