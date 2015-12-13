70e9893bbd8a43e3f6f3dfe19e2a746bd407026e
ezhulkov <ezhulkov@gmail.com>
2015-12-13 14:01:14 +0300

    + показывать больше инфы о тендере на главной странице

--- gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/AbstractOrder.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/AbstractOrder.java
@@ -79,7 +79,7 @@ public abstract class AbstractOrder extends BaseComponent {
     }
 
     public boolean isContactsAllowed() {
-        return order == null || order.isContactsAllowed(getAuthenticatedUserSafe());
+        return isAdmin() || order == null || order.isContactsAllowed(getAuthenticatedUserSafe());
     }
 
     public boolean isCartOrOrder() {
