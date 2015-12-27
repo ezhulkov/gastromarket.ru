f430c1b9a5e7c26048b908a0595e03745dd245d9
ezhulkov <ezhulkov@gmail.com>
2015-12-27 16:08:56 +0300

    + поиск товаров по названию

---
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/SearchSection.java
@@ -0,0 +1,41 @@
+package org.ohm.gastro.gui.components;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.ohm.gastro.gui.mixins.BaseComponent;
+import org.ohm.gastro.gui.pages.product.Search;
+
+/**
+ * Created by ezhulkov on 30.05.15.
+ */
+public class SearchSection extends BaseComponent {
+
+    @Property
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private String additionalClass;
+
+    @Property
+    @Parameter(defaultPrefix = BindingConstants.PROP)
+    private String searchString;
+
+    public Object onSubmitFromSearchForm() {
+        System.out.println(searchString);
+        if (StringUtils.isNotEmpty(searchString)) {
+            return getPageLinkSource().createPageRenderLinkWithContext(Search.class, processSearchString(searchString));
+        } else {
+            return this;
+        }
+    }
+
+    private String processSearchString(String searchString) {
+        if (searchString == null) return "";
+        searchString = searchString.replaceAll(":", "");
+        searchString = searchString.replaceAll(";", "");
+        searchString = searchString.replaceAll("<", "");
+        searchString = searchString.replaceAll(">", "");
+        return searchString;
+    }
+
+}
