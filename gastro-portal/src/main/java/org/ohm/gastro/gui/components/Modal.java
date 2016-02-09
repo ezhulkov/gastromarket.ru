79bbf186353875321a5bfacf73f4b7a6057c9c10
ezhulkov <ezhulkov@gmail.com>
2016-02-09 12:20:26 +0300

    - новые заказы

---
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/Modal.java
@@ -0,0 +1,25 @@
+package org.ohm.gastro.gui.components;
+
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.ohm.gastro.gui.mixins.BaseComponent;
+
+/**
+ * Created by ezhulkov on 14.02.15.
+ */
+public class Modal extends BaseComponent {
+
+    @Property
+    @Parameter(name = "caption", allowNull = false, required = true, defaultPrefix = BindingConstants.LITERAL)
+    private String caption;
+
+    @Property
+    @Parameter(name = "modalId", allowNull = false, required = true, defaultPrefix = BindingConstants.LITERAL)
+    private String modalId;
+
+    @Property
+    @Parameter(name = "additionalClass", allowNull = false, required = false, defaultPrefix = BindingConstants.LITERAL)
+    private String additionalClass;
+
+}
