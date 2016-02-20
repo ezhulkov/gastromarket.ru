5e947daafc0b60850ddbe193bb5eb2fad43b53e5
ezhulkov <ezhulkov@gmail.com>
2016-02-20 17:41:17 +0300

    - новые заказы

--- gastro-portal/src/main/java/org/ohm/gastro/gui/components/comment/Comment.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/comment/Comment.java
@@ -1,21 +1,20 @@
 package org.ohm.gastro.gui.components.comment;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.Link;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.TextArea;
 import org.apache.tapestry5.corelib.components.TextField;
-import org.ohm.gastro.domain.CatalogEntity;
 import org.ohm.gastro.domain.CommentEntity;
 import org.ohm.gastro.domain.CommentableEntity;
 import org.ohm.gastro.domain.OrderEntity;
-import org.ohm.gastro.domain.UserEntity;
 import org.ohm.gastro.gui.mixins.BaseComponent;
 import org.ohm.gastro.gui.pages.office.Order;
-
-import java.util.Optional;
 
 /**
  * Created by ezhulkov on 14.02.15.
@@ -37,6 +36,9 @@ public class Comment extends BaseComponent {
     @Property
     private String attachReason;
 
+    @Property
+    private String totalPrice;
+
     @Parameter(defaultPrefix = BindingConstants.LITERAL, value = "true")
     private boolean showEdit;
 
@@ -47,18 +49,10 @@ public class Comment extends BaseComponent {
     private TextField mobilePhone;
 
     @Component(id = "deliveryAddress", parameters = {"value=authenticatedUser.deliveryAddress", "validate=required"})
-    private TextField deliveryAddress;
+    private TextArea deliveryAddress;
 
     @Component
     private Form attachTenderAjaxForm;
-
-    private Optional<CatalogEntity> getFirstCatalog() {
-        return getFirstCatalog(comment.getAuthor());
-    }
-
-    private Optional<CatalogEntity> getFirstCatalog(UserEntity cook) {
-        return getCatalogService().findAllCatalogs(cook).stream().findFirst();
-    }
 
     public boolean isCookReply() {
         return comment.getAuthor().isCook();
@@ -69,15 +63,29 @@ public class Comment extends BaseComponent {
     }
 
     public boolean isOrderOwner() {
-        return isAdmin() || getOrder().isOrderOwner(getAuthenticatedUserSafe());
+        return getOrder().isOrderOwner(getAuthenticatedUserSafe());
+    }
+
+    public void onPrepareFromAttachTenderAjaxForm(Long uid, Long oid, Long cid) {
+        if (getOrder() != null) {
+            comment = getConversationService().findComment(cid);
+            totalPrice = ObjectUtils.defaultIfNull(getOrder().getTotalPrice() == null ?
+                                                           comment.getBudget() :
+                                                           getOrder().getTotalPrice(),
+                                                   0)
+                    .toString();
+        }
     }
 
     public Link onSuccessFromAttachTenderAjaxForm(Long uid, Long oid, Long cid) {
+        if (StringUtils.isEmpty(totalPrice)) return null;
+        final int tp = Integer.parseInt(totalPrice);
+        if (tp <= 0) return null;
         if (attachTenderAjaxForm.getHasErrors()) return null;
-        return getFirstCatalog(getUserService().findUser(uid)).map(catalog -> {
+        return getUserService().findUser(uid).getFirstCatalog().map(catalog -> {
             final OrderEntity tender = getOrderService().findOrder(oid);
             tender.setAttachReason(attachReason);
-            if (tender.getTotalPrice() == null) tender.setTotalPrice(getConversationService().findComment(cid).getBudget());
+            tender.setTotalPrice(tp);
             getOrderService().attachTender(catalog, tender, getAuthenticatedUser());
             return getPageLinkSource().createPageRenderLinkWithContext(Order.class, true, tender.getId(), false);
         }).orElse(null);
