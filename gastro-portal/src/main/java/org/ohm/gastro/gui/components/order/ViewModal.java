d0633ad1c628be5f36907c3a2544f1fa9f1dd608
ezhulkov <ezhulkov@gmail.com>
2016-03-10 18:35:15 +0300

    - список счетов в админке

--- gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/ViewModal.java
+++ gastro-portal/src/main/java/org/ohm/gastro/gui/components/order/ViewModal.java
@@ -1,13 +1,9 @@
 package org.ohm.gastro.gui.components.order;
 
-import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 import org.ohm.gastro.domain.CatalogEntity;
 import org.ohm.gastro.domain.OrderProductEntity;
 import org.ohm.gastro.gui.mixins.BaseComponent;
-
-import java.util.List;
 
 /**
  * Created by ezhulkov on 31.07.15.
@@ -17,20 +13,8 @@ public class ViewModal extends BaseComponent {
     @Property
     private OrderProductEntity item;
 
-    @Property
-    private CatalogEntity catalog;
-
-    @Property
-    @Inject
-    private Block orderShowBlock;
-
-    public void beginRender() {
-        catalog = getShoppingCart().getLastItem().getEntity().getCatalog();
-    }
-
-    public boolean isNewOrder() {
-        List<OrderProductEntity> items = getShoppingCart().getItems(catalog);
-        return items.size() == 1 && items.stream().allMatch(t -> t.getCount() == 1);
+    public CatalogEntity getCatalog() {
+        return getShoppingCart().getLastItem().getEntity().getCatalog();
     }
 
     public boolean isManyCatalogs() {
